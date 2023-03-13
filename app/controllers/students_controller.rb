c4d7fec9d70c4a69bda44da4a821a04a9b4c0226
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-13 20:13:48 +0300

    Routing in rails done

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,12 @@
+class StudentsController < ApplicationController
+  def index
+    render json: Student.all
+  end
+
+  def grades
+    render json: Student.all.order("grade DESC")
+  end
+  def highest_grade
+    render json: Student.all.order("grade DESC").first
+  end
+end
